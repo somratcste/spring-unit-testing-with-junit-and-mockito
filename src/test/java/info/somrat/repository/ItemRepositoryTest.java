5b7f5aff28eff64346db6803b8463720aede76f9
somratcste <somratcste@gmail.com>
2021-11-12 00:36:38 +0100

    chore: multiple database connection

--- src/test/java/info/somrat/repository/ItemRepositoryTest.java
+++ src/test/java/info/somrat/repository/ItemRepositoryTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import java.util.List;
 
 @DataJpaTest
+@ActiveProfiles("dev")
 public class ItemRepositoryTest {
 
     @Autowired

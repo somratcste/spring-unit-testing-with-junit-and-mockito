a06a0c7e065629f28f38562171b22d313c351586
somratcste <somratcste@gmail.com>
2021-11-11 11:52:04 +0100

    init: start mockito for mocking test

---
+++ src/test/java/info/somrat/business/SomeBusinessMockTest.java
@@ -0,0 +1,38 @@
+package info.somrat.business;
+
+import info.somrat.data.SomeDataService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class SomeBusinessMockTest {
+
+    @InjectMocks
+    SomeBusinessImpl business;
+
+    @Mock
+    SomeDataService dataServiceMock;
+
+    @Test
+    public void calculateSumUsingDataService_basic() {
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {1,2,3});
+        assertEquals(6,  business.calculateSumUsingDataService());
+    }
+
+    @Test
+    public void calculateSumUsingDataService_empty() {
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { } );
+        assertEquals(0, business.calculateSumUsingDataService());
+    }
+
+    @Test
+    public void calculateSumUsingDataService_oneValue() {
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 5 } );
+        assertEquals(5, business.calculateSumUsingDataService());
+    }
+}

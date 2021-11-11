265d2737568b6017267b49f24efd26fcd6f68197
somratcste <somratcste@gmail.com>
2021-11-11 02:10:37 +0100

    chore: init testing application

---
+++ src/main/java/info/somrat/business/SomeBusinessImpl.java
@@ -0,0 +1,10 @@
+package info.somrat.business;
+
+import java.util.Arrays;
+
+public class SomeBusinessImpl {
+
+    public int calculateSum(int[] data) {
+        return Arrays.stream(data).sum();
+    }
+}

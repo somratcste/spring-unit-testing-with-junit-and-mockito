dbc655665323da955ae3e6d0bbcc8d1afaec345b
somratcste <somratcste@gmail.com>
2021-11-12 00:40:42 +0100

    task: refactor code

--- src/test/java/info/somrat/controller/HelloWorldControllerTest.java
+++ src/test/java/info/somrat/controller/HelloWorldControllerTest.java
@@ -3,20 +3,15 @@ package info.somrat.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(HelloWorldController.class)
-// @ComponentScan(basePackages = "info.somrat")
-//@Import(HelloWorldController.class)
 public class HelloWorldControllerTest {
 
     @Autowired
--- src/test/java/info/somrat/controller/ItemControllerTest.java
+++ src/test/java/info/somrat/controller/ItemControllerTest.java
@@ -7,20 +7,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import java.util.Arrays;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ItemController.class)
-//@Import(ItemController.class)
 public class ItemControllerTest {
 
     @Autowired

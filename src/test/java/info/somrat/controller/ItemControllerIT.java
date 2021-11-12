ee849e20f4f9d2031f86a901ca753f44aa2b48d5
somratcste <somratcste@gmail.com>
2021-11-12 01:31:04 +0100

    task: refactor code

--- src/test/java/info/somrat/controller/ItemControllerIT.java
+++ src/test/java/info/somrat/controller/ItemControllerIT.java
@@ -7,10 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("dev")
 public class ItemControllerIT {
 
     @Autowired

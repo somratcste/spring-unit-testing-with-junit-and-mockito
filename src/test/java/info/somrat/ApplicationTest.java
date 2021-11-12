462db04474715eb0c726a07c452142fc839c1d85
somratcste <somratcste@gmail.com>
2021-11-12 02:30:25 +0100

    chore: complex unit text pattern

--- src/test/java/info/somrat/ApplicationTest.java
+++ src/test/java/info/somrat/ApplicationTest.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 //@TestPropertySource(locations= {"classpath:test-configuration.properties"})
+// http://xunitpatterns.com/ -> its good to see all kind of unit test pattern
 class ApplicationTest {
 
     @Test
